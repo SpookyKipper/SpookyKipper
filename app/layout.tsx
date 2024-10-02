b7a488a61b9d7759ccb3e77dac7d5f80dafc64f4
Spooky Kipper <88220209+SpookyKipper@users.noreply.github.com>
2024-10-02 17:02:55 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
       >
         {children}
 
-        <Script src="https://cdn.spooky.hk/mainProfileScripts/darkModeFull.js" strategy="lazyOnload"></Script>
+        <Script src="https://cdn.spooky.hk/mainProfileScripts/darkModeFull.js?ver=1" strategy="afterInteractive"></Script>
       </body>
     </html>
   );
